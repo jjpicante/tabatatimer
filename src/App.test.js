6f4ebc96a7eacb364b15978ff0358ef5835d7ad4
jjpicante <jjfiorovic@gmail.com>
2026-07-11 12:46:17 -0300

    asd

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renderiza el temporizador con sus controles', () => {
   render(<App />);
-  expect(screen.getByRole('button', { name: /iniciar/i })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: /^▶\s*Iniciar$/i })).toBeInTheDocument();
   expect(screen.getByText(/Tiempo restante/i)).toBeInTheDocument();
 });
